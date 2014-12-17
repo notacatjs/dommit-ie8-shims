ec2292cda5d140ef87bfe31a0d38955b69ee0a26
Mike Holm and Kiran Surdhar <mholm@rallydev.com>
2014-12-17 10:34:21 -0700

    fix semicolons

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 if (/MSIE 8.0/.test(navigator.userAgent)) {
   require('es5-shim');
   require('es5-shim/es5-sham');
-  require('./shims/event_listener')
+  require('./shims/event_listener');
   require('./shims/slice_nodelist');
   require('./shims/text_content');
 }
